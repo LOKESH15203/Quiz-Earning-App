493b1037e1544986fd3305a5c945b3e232178f4d
lokesh-flipick <lokesh.sonawane@flipick.com>
2023-08-06 16:29:51 +0530

    Making Fragments

--- app/src/main/java/com/example/quizearningapp/HomeActivity.kt
+++ app/src/main/java/com/example/quizearningapp/HomeActivity.kt
@@ -2,10 +2,17 @@ package com.example.quizearningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        var NavController = findNavController(R.id.fragmentContainerView)
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setupWithNavController(NavController)
     }
 }
