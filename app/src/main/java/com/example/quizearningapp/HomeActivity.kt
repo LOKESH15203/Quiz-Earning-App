d1e5e0546642c4106ec8b307d4b2bd8b229e19b2
lokesh-flipick <121492333+LOKESH15203@users.noreply.github.com>
2023-10-02 09:51:48 +0530

    Complete implementation of it menu NAVIGATION

--- app/src/main/java/com/example/quizearningapp/HomeActivity.kt
+++ app/src/main/java/com/example/quizearningapp/HomeActivity.kt
@@ -1,18 +1,44 @@
 package com.example.quizearningapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.quizearningapp.databinding.ActivityHomeBinding
+import com.example.quizearningapp.fragments.HistoryFragment
+import com.example.quizearningapp.fragments.HomeFragment
+import com.example.quizearningapp.fragments.ProfileFragment
+import com.example.quizearningapp.fragments.SpinFragment
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        var NavController = findNavController(R.id.fragmentContainerView)
-        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNav.setupWithNavController(NavController)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.homeFragment -> replaceFragment(HomeFragment())
+                R.id.historyFragment -> replaceFragment(HistoryFragment())
+                R.id.spinFragment -> replaceFragment(SpinFragment())
+                R.id.profileFragment -> replaceFragment(ProfileFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)
+        fragmentTransaction.commit()
     }
 }
